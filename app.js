9e3858eae0dc90994432cc4aa8375025cdc077ec
Greg <Ghughes610@gmail.com>
2021-05-10 18:02:21 -0600

    Added db.json file

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-Hi
+const playersURL = 'http://localhost:3000/players'
 
-hey
+fetch(playersURL)
+    .then(response => response.json())
+    .then(players => {
+        players.forEach(player => {
+            const li = document.createElement('li')
+            li.innerText = `${player.firstName} ${player.lastName}`
+            console.log(li)
+        })
+    })
