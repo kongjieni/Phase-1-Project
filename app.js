a6c15a2d86e32764cff4603b0fa84b5361945702
Jay Ni <signed121@gmail.com>
2021-05-11 14:23:23 -0600

    Edited css for easy.html

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-
 const playersURL = 'http://localhost:3000/players'
+const playerContainer = document.querySelector('.container')
+const array = []
 
 fetch(playersURL)
     .then(response => response.json())
     .then(players => {
         players.forEach(player => {
-            const li = document.createElement('li')
-            li.innerText = `${player.firstName} ${player.lastName}`
-            console.log(li)
+
+            if (player.difficulty === 'easy') {
+                const easyPlayers = document.createElement('li')
+                easyPlayers.innerText = `${player.firstName} ${player.lastName}`
+                playerContainer.append(easyPlayers)
+            } 
+            
+
+            // const playerStats = document.createElement('ul')
+            // playerStats.innerText = ''
+
+            // const playerNumber = document.createElement('li')
+            // playerNumber.innerText = player.numbers
+
+            // const playerTeams = document.createElement('li')
+            // playerTeams.innerText = player.teams
+
+            // const playerDifficulty = document.createElement('li')
+            // playerDifficulty.innerText = player.difficulty
+
+            // playerStats.append(playerName, playerTeams, playerNumber, playerDifficulty)
+            // // if (player.difficulty === 'easy') {
+            // //     const easyPlayers = document.createElement('li')
+            // //     easyPlayers.innerText = playerName
+            // //     playerContainer.append(easyPlayers)
+            
         })
     })
-
