18beeed51ea0f2c620ea1cc90b916d9ecdf9e32c
Jay Ni <signed121@gmail.com>
2021-05-11 15:13:49 -0600

    same page

--- app.js
+++ app.js
@@ -1,109 +1,44 @@
 const playersURL = 'http://localhost:3000/players'
-const playerContainer = document.querySelector('.container')
 const array = []
 const easyContainer = document.querySelector('#easyContainer')
-// const easyButton = document.
+const wrongAnswerButton = document.querySelectorAll('answer-btn')
+
 
 fetch(playersURL)
     .then(response => response.json())
     .then(players => {
+        const easyPlayers = players.splice(0, 7)
+
         players.forEach(player => {
 
-            if (player.difficulty === 'easy') {
-                const easyPlayers = document.createElement('li')
-                easyPlayers.innerText = `${player.firstName} ${player.lastName}`
-                playerContainer.append(easyPlayers)
-            } 
-            
+            easyPlayers.forEach(player => {
 
-            // const playerStats = document.createElement('ul')
-            // playerStats.innerText = ''
+                const playerCard = document.createElement('ul')
+                const title = document.createElement('h3')
 
-            // const playerNumber = document.createElement('li')
-            // playerNumber.innerText = player.numbers
+                title.innerText = `${player.firstName} ${player.lastName}`
 
-            // const playerTeams = document.createElement('li')
-            // playerTeams.innerText = player.teams
+                const playerImage = document.createElement('img')
 
-            // const playerDifficulty = document.createElement('li')
-            // playerDifficulty.innerText = player.difficulty
+                playerImage.className = 'imagesOfPlayers'
 
-            // playerStats.append(playerName, playerTeams, playerNumber, playerDifficulty)
-            // // if (player.difficulty === 'easy') {
-            // //     const easyPlayers = document.createElement('li')
-            // //     easyPlayers.innerText = playerName
-            // //     playerContainer.append(easyPlayers)
-            
+                playerImage.src = player.image
+
+                const playerDifficulty = document.createElement('li')
+
+                playerDifficulty.innerText = player.difficulty
+
+                const playerTeams = document.createElement('li')
+
+                playerTeams.innerText = player.teams
+
+                const playerNumber = document.createElement('li')
+
+                playerNumber.innerText = `# ${player.numbers}`
+
+                playerCard.append(title, playerNumber, playerTeams, playerDifficulty, playerImage)
+
+                easyContainer.append(playerCard)
+            })
         })
     })
-        const easyPlayers = players.splice(0, 7)
-        const mediumPlayers = players.splice(0, 7)
-        const epicPlayers = players
-
-
-        easyPlayers.forEach(player => {
-          
-            const playerCard = document.createElement('ul')
-
-            const title = document.createElement('h3')
-            title.innerText = `${player.firstName} ${player.lastName}`
-
-            const playerImage = document.createElement('img')
-            playerImage.className = 'imagesOfPlayers'
-            playerImage.src = player.image
-            
-            const playerDifficulty = document.createElement('li')
-            playerDifficulty.innerText = player.difficulty
-
-            const playerTeams = document.createElement('li')
-            playerTeams.innerText = player.teams
-
-            const playerNumber = document.createElement('li')
-            playerNumber.innerText = `# ${player.numbers}`
-
-            playerCard.append(title, playerNumber, playerTeams, playerDifficulty, playerImage)
-            easyContainer.append(playerCard)
-
-        })
-    })
-    // mediumPlayers.forEach(player => {
-
-        //     const mediumPlayerCard = document.createElement('ul')
-
-        //     const title = document.createElement('h3')
-        //     title.innerText = `${player.firstName} ${player.lastName}`
-
-        //     const playerDifficulty = document.createElement('li')
-        //     playerDifficulty.innerText = player.difficulty
-
-        //     const playerTeams = document.createElement('li')
-        //     playerTeams.innerText = player.teams
-
-        //     const numberLi = document.createElement('li')
-        //     numberLi.innerText = player.numbers
-
-        //     mediumPlayerCard.append(title, playerTeams, numberLi, playerDifficulty)
-        //     mediumContainer.append(mediumPlayerCard)
-
-        // })
-
-        // epicPlayers.forEach(player => {
-
-        //     const epicPlayerCard = document.createElement('ul')
-
-        //     const title = document.createElement('h3')
-        //     title.innerText = `${player.firstName} ${player.lastName}`
-
-        //     const playerDifficulty = document.createElement('li')
-        //     playerDifficulty.innerText = player.difficulty
-
-        //     const playerTeams = document.createElement('li')
-        //     playerTeams.innerText = player.teams
-
-        //     const numberLi = document.createElement('li')
-        //     numberLi.innerText = player.numbers
-
-        //     epicPlayerCard.append(title, playerTeams, numberLi)
-        //     // console.log(epicPlayerCard)
-
-        // })
