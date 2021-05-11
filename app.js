8a405c5c64e69b73042aa1fce77af1917695f6d8
Greg <Ghughes610@gmail.com>
2021-05-11 14:14:37 -0600

    JS for Dom

--- app.js
+++ app.js
@@ -1,37 +1,79 @@
 
 const playersURL = 'http://localhost:3000/players'
+const easyContainer = document.querySelector('#easyContainer')
+// const easyButton = document.
 
 fetch(playersURL)
     .then(response => response.json())
     .then(players => {
-        players.forEach(player => {
-            const easyContainer = document.getElementsByClassName('.easyContainer')
-            // console.log(easyContainer)
+        const easyPlayers = players.splice(0, 7)
+        const mediumPlayers = players.splice(0, 7)
+        const epicPlayers = players
 
-            const playerName = document.createElement('h3')
-            playerName.innerText = `${player.firstName} ${player.lastName}`
 
-            const playerStats = document.createElement('ul')
-            playerStats.innerText = ''
+        easyPlayers.forEach(player => {
+          
+            const playerCard = document.createElement('ul')
 
-            const playerNumber = document.createElement('li')
-            playerNumber.innerText = player.numbers
+            const title = document.createElement('h3')
+            title.innerText = `${player.firstName} ${player.lastName}`
+
+            const playerImage = document.createElement('img')
+            playerImage.className = 'imagesOfPlayers'
+            playerImage.src = player.image
+            
+            const playerDifficulty = document.createElement('li')
+            playerDifficulty.innerText = player.difficulty
 
             const playerTeams = document.createElement('li')
             playerTeams.innerText = player.teams
 
-            const playerDifficulty = document.createElement('li')
-            playerDifficulty.innerText = player.difficulty
+            const playerNumber = document.createElement('li')
+            playerNumber.innerText = `# ${player.numbers}`
 
-            playerStats.append(playerName, playerTeams, playerNumber, playerDifficulty)
-            // console.log(player.difficulty)
+            playerCard.append(title, playerNumber, playerTeams, playerDifficulty, playerImage)
+            easyContainer.append(playerCard)
 
-            if (player.difficulty === 'easy') {
-                const easyPlayers = document.createElement('li')
-                easyPlayers.innerText = player.firstName
-                // easyContainer.append(easyPlayers)
-                console.log(easyContainer)
-            }
         })
     })
+    // mediumPlayers.forEach(player => {
 
+        //     const mediumPlayerCard = document.createElement('ul')
+
+        //     const title = document.createElement('h3')
+        //     title.innerText = `${player.firstName} ${player.lastName}`
+
+        //     const playerDifficulty = document.createElement('li')
+        //     playerDifficulty.innerText = player.difficulty
+
+        //     const playerTeams = document.createElement('li')
+        //     playerTeams.innerText = player.teams
+
+        //     const numberLi = document.createElement('li')
+        //     numberLi.innerText = player.numbers
+
+        //     mediumPlayerCard.append(title, playerTeams, numberLi, playerDifficulty)
+        //     mediumContainer.append(mediumPlayerCard)
+
+        // })
+
+        // epicPlayers.forEach(player => {
+
+        //     const epicPlayerCard = document.createElement('ul')
+
+        //     const title = document.createElement('h3')
+        //     title.innerText = `${player.firstName} ${player.lastName}`
+
+        //     const playerDifficulty = document.createElement('li')
+        //     playerDifficulty.innerText = player.difficulty
+
+        //     const playerTeams = document.createElement('li')
+        //     playerTeams.innerText = player.teams
+
+        //     const numberLi = document.createElement('li')
+        //     numberLi.innerText = player.numbers
+
+        //     epicPlayerCard.append(title, playerTeams, numberLi)
+        //     // console.log(epicPlayerCard)
+
+        // })
