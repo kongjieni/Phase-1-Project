c84f06f111b3ee0a1c153f818d515ad4a3366f32
Greg <Ghughes610@gmail.com>
2021-05-11 10:24:32 -0600

    Added some app.js

--- app.js
+++ app.js
@@ -5,9 +5,33 @@ fetch(playersURL)
     .then(response => response.json())
     .then(players => {
         players.forEach(player => {
-            const li = document.createElement('li')
-            li.innerText = `${player.firstName} ${player.lastName}`
-            console.log(li)
+            const easyContainer = document.getElementsByClassName('.easyContainer')
+            // console.log(easyContainer)
+
+            const playerName = document.createElement('h3')
+            playerName.innerText = `${player.firstName} ${player.lastName}`
+
+            const playerStats = document.createElement('ul')
+            playerStats.innerText = ''
+
+            const playerNumber = document.createElement('li')
+            playerNumber.innerText = player.numbers
+
+            const playerTeams = document.createElement('li')
+            playerTeams.innerText = player.teams
+
+            const playerDifficulty = document.createElement('li')
+            playerDifficulty.innerText = player.difficulty
+
+            playerStats.append(playerName, playerTeams, playerNumber, playerDifficulty)
+            // console.log(player.difficulty)
+
+            if (player.difficulty === 'easy') {
+                const easyPlayers = document.createElement('li')
+                easyPlayers.innerText = player.firstName
+                // easyContainer.append(easyPlayers)
+                console.log(easyContainer)
+            }
         })
     })
 
