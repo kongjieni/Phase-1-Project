a863b742524aafb9ba06ff01995c9f904373a213
Jay Ni <signed121@gmail.com>
2021-05-12 09:56:05 -0600

    Refactored code

--- app.js
+++ app.js
@@ -1,88 +1,69 @@
 const playersURL = 'http://localhost:3000/players'
 const container = document.querySelector('.container')
 const question = document.querySelector('#question')
+const easyPlayers = []
+let currentPlayer = 0
+let score = 0
+let correctAnswer = 0
 const answerButton1 = document.querySelector('.answer-btn1')
 const answerButton2 = document.querySelector('.answer-btn2')
 const answerButton3 = document.querySelector('.answer-btn3')
 const answerButton4 = document.querySelector('.answer-btn4')
+const answerButtons = [
+    answerButton1,
+    answerButton2,
+    answerButton3,
+    answerButton4
+]
 const nextButton = document.querySelector('#next-btn')
-
-
-// answerButton1.addEventListener('click', handleWrongClick)
-// answerButton2.addEventListener('click', handleWrongClick)
-// answerButton3.addEventListener('click', handleCorrectClick)
-// answerButton4.addEventListener('click', handleWrongClick)
-// nextButton.addEventListener('click', setNextQuestion)
-
-
-// function handleWrongClick(button) {
-//     button.preventDefault()
-//     answerButton1.innerText = `${answerButton1.innerText} ❌`
-//     }
-
-
-// function handleCorrectClick(event) {
-//     event.preventDefault()
-//     answerButton3.innerText = `${answerButton3.innerText} ✔️`
-// }
-
-// function setNextQuestion(event) {
-//     event.preventDefault()
-//     question.innerText = 'James Harden - Houston Rockets'
-//     answerButton1.innerText = Math.floor(Math.random() * 50)
-//     answerButton2.innerText = Math.floor(Math.random() * 50)
-//     answerButton3.innerText = Math.floor(Math.random() * 50)
-//     answerButton4.innerText = Math.floor(Math.random() * 50)
-// }
 
 fetch(playersURL)
     .then(response => response.json())
     .then(players => {
-        const easyPlayers = players.splice(0, 7)
-        easyPlayers.forEach(player => {
-
-            const playerCard = document.createElement('ul')
-            const title = document.createElement('h3')
-            title.innerText = `${player.firstName} ${player.lastName}`
-
-            const playerImage = document.createElement('img')
-            playerImage.className = 'imagesOfPlayers'
-            playerImage.src = player.image
-
-            const playerDifficulty = document.createElement('li')
-            playerDifficulty.innerText = player.difficulty
-
-            const playerTeams = document.createElement('li')
-            playerTeams.innerText = player.teams
-
-            playerCard.append(title, playerTeams, playerDifficulty,)
-            answerButton1.addEventListener('click', handleWrongClick)
-            answerButton2.addEventListener('click', handleWrongClick)
-            answerButton3.addEventListener('click', handleCorrectClick)
-            answerButton4.addEventListener('click', handleWrongClick)
-            nextButton.addEventListener('click', setNextQuestion)
-
-
-            function handleWrongClick(button) {
-            button.preventDefault()
-            answerButton1.innerText = `${answerButton1.innerText} ❌`
-             }
-
-
-            function handleCorrectClick(event) {
-             event.preventDefault()
-            answerButton3.innerText = `${answerButton3.innerText} ✔️`
-            }
-
-            function setNextQuestion(event) {
-            event.preventDefault()
-            question.innerText = `${player.firstName} ${player.lastName}`
-            answerButton1.innerText = Math.floor(Math.random() * 50)
-            answerButton2.innerText = Math.floor(Math.random() * 50)
-            answerButton3.innerText = Math.floor(Math.random() * 50)
-            answerButton4.innerText = Math.floor(Math.random() * 50)
+        players.forEach(player => {
+            if (player.difficulty === 'easy') {
+                easyPlayers.push(player)
             }
         })
+        handleQuestion(currentPlayer)
     })
 
 
+nextButton.addEventListener('click', event => {
+    event.preventDefault()
+    if (currentPlayer >= easyPlayers.length - 1) {
+        return handleEnding()
+    }
+    currentPlayer++
+    handleQuestion(currentPlayer)
+})
+
+
+function handleEnding() {
+    answerButton1.remove()
+    answerButton2.remove()
+    answerButton3.remove()
+    answerButton4.remove()
+    nextButton.remove()
+    question.innerText = 'Congratulations!'
+}
+
+function handleQuestion(count) {
+    const playerData = easyPlayers[count]
+    question.innerText = `${playerData.firstName} ${playerData.lastName} - ${playerData.teams[0]}`
+}
+
+
+
+
+// To do list
+
+// get buttons to have numbers, 1 jersey 3 random.
+
+// score - function increment score when correct, 
+
+// interpolate score into results page
+
+// randomize button order
+
+// randomize player order
